662119860946be8c67340c6bde7aa97a311501d0
airdmhund1 <edmund.aduasamoah@bigspark.dev>
2025-08-21 08:38:55 +0100

    removed ui

--- app/ui/src/api.js
+++
@@ -1,7 +0,0 @@
-const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:8080";
-
-export async function fetchJson(path) {
-  const res = await fetch(`${API_BASE}${path}`);
-  if (!res.ok) throw new Error(`${res.status} ${res.statusText}`);
-  return res.json();
-}
